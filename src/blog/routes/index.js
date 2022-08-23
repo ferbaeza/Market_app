932178620b9df9084a72d018f7385e4d8522849a
FerBaeza <fbaezahurtado@gmail.com>
2022-08-23 16:47:38 +0200

    initial installation

--- src/blog/routes/index.js
+++ src/blog/routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const path= require('path')
 
 const PATH_ROUTES=  __dirname;
+
+const path_R =path.parse(__dirname).dir
+const t = path_R.split('/').at(-1)
+
+
 
 const removeExtension=(fileName)=>{
     return fileName.split('.').shift()
@@ -13,7 +19,7 @@ const removeExtension=(fileName)=>{
 fs.readdirSync(PATH_ROUTES).filter((file)=>{
     const name = removeExtension(file)
     if (name !== 'index'){
-        console.log(`Cargando las rutas de ${name}`)
+        console.log(`Cargando las rutas de ${t}/${name}`)
         router.use(`/${name}`, require(`./${file}`))
     }
 })
